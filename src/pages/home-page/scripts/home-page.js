7b804082659053c3c0064f9d2eeaf1052b6ec572
Bartosz Zawadka <bart.zaw00@gmail.com>
2023-03-10 19:06:15 +0100

    Desktop version almost done, stroke color of svg icon to do

---
+++ src/pages/home-page/scripts/home-page.js
@@ -0,0 +1,10 @@
+/// Homepage JavaScript File
+/// Here we import all the JavaScript files we need for our homepage.
+
+import '../styles/benefit-section.scss'
+import '../styles/benefit-box.scss'
+import '../styles/benefit-content.scss'
+
+import { setupCounter } from '../../../global-scripts/scripts/counter/counter.js'
+
+setupCounter(document.querySelector('#counter'))
